67e4bc615eff5d5af78a3c8bc186c352e0ea00e8
Ms0232 <180540106059@darshan.ac.in>
2023-02-17 17:40:42 +0530

    main

--- src/index.js
+++ src/index.js
@@ -10,20 +10,21 @@ import { Auth0Provider } from "@auth0/auth0-react";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  // <Auth0Provider
-  //   domain={process.env.REACT_APP_AUTH_DOMAIN}
-  //   clientId={process.env.REACT_APP_CLIENT_ID}
-  //   redirectUri={window.location.origin}
-  //   cacheLocation='localstorage'
-  // >
-  // <UserProvider>
-  <ProductsProvider>
-    <FilterProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </FilterProvider>
-  </ProductsProvider>
-  // </UserProvider>
-  // </Auth0Provider>
+  <Auth0Provider
+    domain={process.env.REACT_APP_AUTH_DOMAIN}
+    clientId={process.env.REACT_APP_CLIENT_ID}
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
+    <UserProvider>
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </UserProvider>
+  </Auth0Provider>
 );
